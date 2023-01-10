de0bedd4d1e006365f2416ba43072d60a278de64
shulgin3@gmail.com <shulgin3@gmail.com>
2023-01-10 20:47:06 +0700

    10/01/23

--- js/lesson1/script.js
+++ js/lesson1/script.js
@@ -1,5 +1,7 @@
 // let answer = confirm('Вы здесь?');
 // console.log(answer);
 
-// let answer = prompt('Есть ли Вам 18?', 'Да');
+// let answer = +prompt('Есть ли Вам 18?', 'Да');
 // console.log(typeof(answer));
+//console.log('Меня зовут' + 'Максим');
+//console.log(5 + 'Максим');
