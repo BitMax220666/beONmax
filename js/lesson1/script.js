09085af596d90a80330f8817b89a86980f02ba54
shulgin3@gmail.com <shulgin3@gmail.com>
2023-01-12 20:09:34 +0700

    12/01/23

--- js/lesson1/script.js
+++ js/lesson1/script.js
@@ -5,10 +5,18 @@
 // console.log(typeof(answer));
 //console.log('Меня зовут' + 'Максим');
 //console.log(5 + 'Максим');
-let incr = 10,
-    decr = 10;
+// let incr = 10,
+//     decr = 10;
 
-console.log(incr++);
-console.log(decr--);
+// console.log(incr++);
+// console.log(decr--);
 
-console.log(5 % 2);
+// console.log(5 % 2);
+
+// console.log('2' == 2);
+// console.log('2' === 2);
+
+let open = true,
+    close = true;
+
+console.log(open && close);
