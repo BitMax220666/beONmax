4aa7582d7d1826f35428e786343741d5a78de2ef
Maksim <shulgin3@gmail.com>
2023-02-05 16:03:25 +0700

    05/02/23

--- js/lesson1/script.js
+++ js/lesson1/script.js
@@ -2,6 +2,20 @@ let money = prompt('Ваш бюджет на месяц?', ''),
     time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
 let appData = {
-    money: money,
+    bydjet: money,
+    expenses: {},
+    optionalExpenses: {},
+    income: {},
     timeData: time,
+    saving: false,
 };
+
+let a1 = prompt('Введите обязательную статью расходов в этом месяце', ''),
+a2 = prompt('Во сколько обойдется?', ''),
+a3 = prompt('Введите обязательную статью расходов в этом месяце', ''),
+a4 = prompt('Во сколько обойдется?', '');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.bydjet / 30);
