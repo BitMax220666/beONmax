3b3374a04801a854d3af31fdeb994425cbb116e6
Maksim <shulgin3@gmail.com>
2023-02-12 11:22:31 +0700

    12/02/23

--- test2/script2.js
+++ test2/script2.js
@@ -1,3 +1,5 @@
+'use strict';
+
 let money = +prompt('Ваш бюджет на месяц?', ''),
     time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
@@ -16,18 +18,60 @@ for (let i = 0; i < 2; i++) {
 
     if ( (typeof(a))==='string' && (typeof(a) != null && typeof(b) != null
     && a != '' && b != '' && a.length < 50)) {
-        appData.expenses[a] = b;
         console.log('done');
+        appData.expenses[a] = b;
     } else {
-
+        console.log('bad result');
+        i--;
     }
-
-    
 };
+
+// Используем цикл WHILE
+
+// let i = 0;
+// while (i < 2) {
+//     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+//         b = prompt ("Во сколько обойдется?", "");
+
+//     if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+
+//         console.log ("done");
+
+//         appData.expenses[a] = b;
+//     } else {
+//          console.log ("bad result");
+//          i--;
+//     }
+
+//     i++;
+// }
+
+
+
+// Используем цикл DO...WHILE
+
+// let i = 0;
+// do {
+//     let a = prompt ("Введите обязательную статью расходов в этом месяце", ""),
+//         b = prompt ("Во сколько обойдется?", "");
+
+//     if ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50) {
+
+//         console.log ("done");
+
+//         appData.expenses[a] = b;
+//     } else {
+//          console.log ("bad result");
+//          i--;
+//     }
+
+//     i++;
+// }
+// while(i < 2);
 
 appData.moneyPerDay = appData.bydjet / 30;
 
-alert('Ежедневный бюджет: ' + appData.moneyPerDay);
+alert('Ежедневный бюджет: ' + appData.moneyPerDay + 'руб.');
 
 if (appData.moneyPerDay < 100) {
     console.log('Минимальный уровень достатка');
